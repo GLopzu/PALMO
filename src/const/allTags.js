6314d303387841bd6389af441748945cc712796c
Marinseal <124858206+Marinseal@users.noreply.github.com>
2024-05-10 17:07:51 -0500

    Delete src directory

--- src/const/allTags.js
+++
@@ -1,8 +0,0 @@
-
-  const allTags = [
-    "UX Research", "UI Design", "Branding", "Post production", "Lettering",
-    "Backend", "Frontend", "Illustration", "Motion Graphics", "Animation",
-    "Photography", "3D Modeling"
-  ];
-
-  export default allTags;
